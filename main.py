c1f2e849102350096ebc3b64fbaaabb14a6233d7
angelopol <114349306+angelopol@users.noreply.github.com>
2025-02-11 09:15:39 -0400

    first example

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def first_example():
+    '''
+        FG Example First Fast API Example 
+    '''
+    return {"GFG Example": "FastAPI"}
